364912b7a9b072a792a6d39282cfe52eb2d57fe1
Mike Murphy <mikeymurph77@gmail.com>
2014-07-21 15:33:47 -0400

    Create Region and Category fields

---
+++ app/controllers/admin_options_controller.rb
@@ -0,0 +1,5 @@
+class AdminOptionsController < ApplicationController
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
+require "monban/constraints/signed_in"
+
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
+
+  constraints Monban::Constraints::SignedIn.new do
+    get "/", to: "admin_options#index", as: :admin_option
+  end
 
   root to: "sessions#new"
 end
