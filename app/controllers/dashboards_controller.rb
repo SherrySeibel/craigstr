8750afa324abbf81d6ea47c999d1967589b03a71
Mike Murphy <mikeymurph77@gmail.com>
2014-07-22 15:13:48 -0400

    Add index for uniqueness in regions migration

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,4 @@
 class DashboardsController < ApplicationController
   def index
-    regions = Region.all
   end
 end
--- db/migrate/20140722134836_create_regions.rb
+++ db/migrate/20140722134836_create_regions.rb
@@ -1,9 +1,11 @@
 class CreateRegions < ActiveRecord::Migration
   def change
     create_table :regions do |t|
-      t.string :name, null: false, unique: true
+      t.string :name, null: false
 
       t.timestamps null: false 
     end
+
+    add_index :regions, [:name], unique: true
   end
 end
